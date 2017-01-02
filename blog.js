a4cb437952de1cc339f9593f7b874d5e1825ea5d
Genijalac <portalgenijalac@gmail.com>
2017-01-02 16:42:32 +0100

    ''

--- blog.js
+++ blog.js
@@ -1,12 +1,16 @@
 var fsx = require('fs-extra');
 var fs = require('fs');
 var DB = require('node-json-db');
+var _ = require('underscore');
 var yamlFront = require('yaml-front-matter');
-var dev = true;
+var dev = false;
 var assets = (!dev) ? 'src/assets/data' : 'DB';
-
+var metaDB = new DB(assets + "/meta", true, false);
 // PRETRAŽUJE SVE FILOVE U DATABASE FOLDERU
 var files = fsx.walkSync('database/');
+var dataStart = '/data/';
+var objectRepresentation = [];
+var predmeti = ["mat1", "mat2", "mat3", "mat4", "fiz2"];
 
 (function kreirajLekcije() {
     for (var i = 0; i < files.length; i++) {
@@ -14,12 +18,92 @@ var files = fsx.walkSync('database/');
         var data = yamlFront.loadFront(fs.readFileSync(files[i], 'utf-8'));
         // AKO FILE SADRŽI .yaml
         if (files[i].indexOf('.yaml') !== -1) {
-            var url = '/' + data["predmet"].toLowerCase() + '/' + data["cjelina"].toLowerCase() + '/' + data["slag"].toLowerCase();
+
+            var placeholder = files[i].split("\\");
+            /// 1 - predmet , 2 - cjelina, 3 - lekicja, 4 - jedinica
+            var predmetLink = placeholder[1].toLowerCase();
+            var cjelinaLink = placeholder[2].toLowerCase().replace(/\s/gi, "-").replace(/ž/gi, "z").replace(/š/gi, "s").replace(/đ/gi, "d").replace(/č/gi, "c").replace(/ć/gi, "c");
+            var lekcijaLink = placeholder[3].toLowerCase().replace(/\s/gi, "-").replace(/ž/gi, "z").replace(/š/gi, "s").replace(/đ/gi, "d").replace(/č/gi, "c").replace(/ć/gi, "c");
+            var jedinicaLink = placeholder[4].toLowerCase().replace(/\s/gi, "-").replace(/ž/gi, "z").replace(/š/gi, "s").replace(/đ/gi, "d").replace(/č/gi, "c").replace(/ć/gi, "c").replace(".yaml", "");
+            var url = '/' + predmetLink + '/' + cjelinaLink + '/' + lekcijaLink + '/' + jedinicaLink;
             var db = new DB(assets + url, true, false);
             db.push('/', data)
+
+            var jedinicaMeta = { "jedinica": [data["naslov"], jedinicaLink], "tip": data["tip"] };
+            var lekcijaMeta = { "lekcija": [placeholder[3], lekcijaLink] };
+            var cjelinaMeta = { "cjelina": [placeholder[2], cjelinaLink], "data": [] };
+
+
+
+            objectRepresentation.push({
+                predmetLink: predmetLink,
+                predmetName: placeholder[1],
+                cjelinaLink: cjelinaLink,
+                cjelinaName: placeholder[2],
+                lekcijaLink: lekcijaLink,
+                lekcijaName: placeholder[3],
+                jedinicaLink: jedinicaLink,
+                jedinicaName: data["naslov"],
+                jedinicaTip: data["tip"],
+                cjelinaMeta: cjelinaMeta,
+                lekcijaMeta: lekcijaMeta,
+                jedinicaMeta: jedinicaMeta
+
+            })
+
+            metaDB.push(dataStart + predmetLink, { "predmet": [predmetName(predmetLink), predmetLink], "cjeline": [] });
         }
     }
-    fsx.copy('database/meta.json', assets + '/meta.json', function(err, data) {
-        if (err) { console.log(err); }
-    })
 })();
+
+
+for (var i = 0; i < predmeti.length; i++) {
+
+    var jedinice = _.where(objectRepresentation, { "predmetLink": predmeti[0] });
+    var shema = [];
+    var grupiraneCjeline = _.groupBy(jedinice, 'cjelinaName')
+    console.log(grupiraneCjeline);
+    _.each(grupiraneCjeline, function(item, key, list) {
+        var cjelina = { "cjelina": [key, key.toLowerCase().replace(/\s/gi, "-").replace(/ž/gi, "z")], data: [] };
+        var lekcije = getLekcijeFromCjeline(item);
+        _.each(lekcije, function(jedinice, imeLekcije) {
+            cjelina["data"].push({ "lekcija": [imeLekcije, imeLekcije.toLowerCase().replace(/\s/gi, "-").replace(/ž/gi, "z")], data: getJediniceFromCjelineLekcije(jedinice) })
+        })
+        shema.push(cjelina)
+    })
+    metaDB.push(dataStart + predmeti[0] + '/cjeline', shema)
+}
+
+function getLekcijeFromCjeline(jedinice) {
+    // jedinice koje su iz iste cjeline
+    var grupirane = _.groupBy(jedinice, "lekcijaName");
+    return grupirane;
+}
+
+function getJediniceFromCjelineLekcije(jedinice) {
+    var data = [];
+    _.each(jedinice, function(value, key) {
+        data.push({ "jedinica": [value["jedinicaName"], value["jedinicaLink"]], "tip": value["jedinicaTip"] })
+    })
+    return data;
+}
+
+function predmetName(link) {
+    switch (link) {
+        case "mat1":
+            return "Matematika 1";
+            break;
+        case "mat2":
+            return "Matematika 2";
+            break;
+        case "mat3":
+            return "Matematika 3";
+            break;
+        case "mat4":
+            return "Matematika 4";
+            break;
+        case "fiz2":
+            return "Fizika 2";
+            break;
+    }
+}
